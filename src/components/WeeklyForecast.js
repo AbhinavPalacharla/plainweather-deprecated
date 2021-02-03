271bca02fc0bf2122f3f748507d51a2fc01a9e13
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2021-02-03 15:51:01 -0800

    fixed centering on columns in weekly forecast

--- src/components/WeeklyForecast.js
+++ src/components/WeeklyForecast.js
@@ -6,7 +6,7 @@ const WeeklyForecast = props => {
   for (let i = 0; i < props.forecast.length; i++) {
     elements.push(
       <li key={props.forecast[i].day}>
-        <div className="flex flex-col content-center px-8">
+        <div className="flex flex-col place-items-center px-8">
           <h1 className="font-light">{props.forecast[i].abbrev_day}</h1>
           <p className="font-semibold pt-2">
             {props.forecast[i].tempHigh + "°"}
